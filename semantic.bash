cbc0294c5e97206e5427ecfda3be360357440d0e
fur <ljq435@sjtu.edu.cn>
2019-03-27 00:05:20 +0800

    build

--- semantic.bash
+++ semantic.bash
@@ -5,5 +5,5 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin com.fur.Main"
+export CCHK="java -classpath ./lib/antlr-4.7.2.jar:./bin com.fur.Main"
 $CCHK
