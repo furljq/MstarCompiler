9084b989ce63654351a2e02ae211e84c3e557062
fur <ljq435@sjtu.edu.cn>
2019-03-30 00:41:52 +0800

    wrap if and loop statement

--- src/com/fur/Main.java
+++ src/com/fur/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         String mstarFile = "main.mx";
-        Compiler compiler = new Compiler(mstarFile);
-//        Compiler compiler = new Compiler();
+//        Compiler compiler = new Compiler(mstarFile);
+        Compiler compiler = new Compiler();
         compiler.compile();
     }
 
