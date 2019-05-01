0316c0b454afdedbc20441a27a99712f3a9f6c5c
fur <ljq435@sjtu.edu.cn>
2019-05-01 18:27:40 +0800

    nasmbuilder finished

--- src/com/fur/nasm/NASMBuilder.java
+++ src/com/fur/nasm/NASMBuilder.java
@@ -1,5 +1,6 @@
 package com.fur.nasm;
 
+import com.fur.enumerate.OperatorList;
 import com.fur.intermediateRepresentation.IRRegister;
 import com.fur.intermediateRepresentation.IntermediateRepresentationBaseVisitor;
 import com.fur.intermediateRepresentation.node.*;
@@ -81,6 +82,52 @@ public class NASMBuilder extends IntermediateRepresentationBaseVisitor<List<Stri
 
     @Override
     public List<String> visitCmpIRNode(CmpIRNode node) {
-        return super.visitCmpIRNode(node);
+        List<String> code = new ArrayList<>();
+        if (node.getOperateIRRegister2().getRegister() == null) {
+            NASMRegister freeRegister = registers.getFreeRegister();
+            if (freeRegister == null) freeRegister = registers.getRegister("r8");
+            code.addAll(freeRegister.load(node.getOperateIRRegister2()));
+        }
+        code.add("cmp\t" + node.getOperateIRRegister1().print() + ", " + node.getOperateIRRegister2().print());
+        String cmpOperator = null;
+        if (node.getOperator() == OperatorList.EQUAL) cmpOperator = "sete";
+        if (node.getOperator() == OperatorList.NOTEQUAL) cmpOperator = "setne";
+        if (node.getOperator() == OperatorList.GT) cmpOperator = "setg";
+        if (node.getOperator() == OperatorList.GEQ) cmpOperator = "setge";
+        if (node.getOperator() == OperatorList.LT) cmpOperator = "setl";
+        if (node.getOperator() == OperatorList.LEQ) cmpOperator = "setle";
+        code.add(cmpOperator + "\t" + node.getDestIRRegister().print());
+        return code;
     }
+
+    @Override
+    public List<String> visitOpIRNode(OpIRNode node) {
+        List<String> code = new ArrayList<>();
+        if (node.getDestIRRegister().getRegister() == null) {
+            NASMRegister freeRegister = registers.getFreeRegister();
+            if (freeRegister == null) freeRegister = registers.getRegister("r8");
+            code.addAll(freeRegister.load(node.getDestIRRegister()));
+        }
+        if (node.getOperator() == OperatorList.MOD ||  node.getOperator() == OperatorList.DIV) {
+            code.addAll(registers.getRegister("rdx").store());
+            code.addAll(registers.getRegister("rax").load(node.getDestIRRegister()));
+            code.add("cqo");
+            code.add("idiv\t" + node.getSourceIRRegister().print());
+            String resultRegister = node.getOperator() == OperatorList.DIV ? "rax" : "rdx";
+            code.add("mov\t" + node.getDestIRRegister().print() + ", " + resultRegister);
+        } else {
+            String operator = null;
+            if (node.getOperator() == OperatorList.ADD) operator = "add";
+            if (node.getOperator() == OperatorList.SUB) operator = "sub";
+            if (node.getOperator() == OperatorList.MUL) operator = "imul";
+            if (node.getOperator() == OperatorList.XOR) operator = "xor";
+            if (node.getOperator() == OperatorList.OR) operator = "or";
+            if (node.getOperator() == OperatorList.AND) operator = "and";
+            if (node.getOperator() == OperatorList.LEFTSHIFT) operator = "sal";
+            if (node.getOperator() == OperatorList.RIGHTSHIFT) operator = "sar";
+            code.add(operator + "\t" + node.getDestIRRegister().print() + ", " + node.getSourceIRRegister());
+        }
+        return code;
+    }
+
 }
